ae189a9cba6cfcee5ce58ec879a1af1d7fd395d7
JacobElbaz <jacobel@post.bgu.ac.il>
2022-07-07 08:24:10 +0300

    create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './E84A2509.jpg';
+import Image from 'react-bootstrap/Image'
+import github from './github.svg'
+import linkedin from './linkedin.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Image src={logo} alt="logo" rounded  width={100} height={100}/>
+        <div className='Titles'>
+          <div className='Up-titles'>
+            <div className='Name'>
+              <h1 className='small mb-0'>Jacob Elbaz</h1>
+              <h2 className='small'>3rd year B.Sc Software engineering student</h2>
+            </div>
+            <div className='links'> 
+              <a href="https://www.linkedin.com/in/jacob-elbaz/" target="_blank" className='text-decoration-none text-white'><img src={linkedin}/> jacob-elbaz</a>
+              <a href="https://github.com/JacobElbaz" target="_blank" className='text-decoration-none text-white'><img src={github}/> JacobElbaz</a>
+            </div>
+          </div>
+          <div className='Motivation'>
+            Motivated and dedicated student who learns quikly and desires to expand his knowledge ans skills
+          </div>
+        </div>
       </header>
+      <body>
+        
+      </body>
     </div>
   );
 }
