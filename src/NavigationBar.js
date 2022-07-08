11e6d5b2c80cdf8ffd3b6800f741ec728665fc81
JacobElbaz <jacobel@post.bgu.ac.il>
2022-07-08 10:35:18 +0300

    test

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,4 +1,4 @@
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import logo from './logo.jpg'
 
 function NavigationBar() {
@@ -6,25 +6,25 @@ function NavigationBar() {
         <Navbar bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand>
-                    <img 
-                    src={logo}
-                    width="40"
-                    height="40"
-                    className="d-inline-block align-top rounded"
-                    alt="Logo" />
+                    <img
+                        src={logo}
+                        width="40"
+                        height="40"
+                        className="d-inline-block align-top rounded"
+                        alt="Logo" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
-                <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll>
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#skills">Skills</Nav.Link>
-                    <Nav.Link href="#projects">Projects</Nav.Link>
-                    <Nav.Link href="#contact">Contact</Nav.Link>
-                </Nav>
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
