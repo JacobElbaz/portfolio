73cc708a7a02e4fbbb275e4bf9be51f36c6ac8b2
JacobElbaz <jacobel@post.bgu.ac.il>
2022-10-26 23:03:53 +0300

    little css changes

--- src/Home.js
+++ src/Home.js
@@ -9,12 +9,12 @@ function Home() {
             <h1 style={{fontSize: "50px"}}>
             Hi, I'm Jacob Elbaz.
             </h1>
-            <h1 style={{color: "aquamarine"}}>
+            <h2 style={{color: "aquamarine"}}>
             Software Engineering Student
-            </h1>
-            <h1 style={{color: "aquamarine"}}>
+            </h2>
+            <h2 style={{color: "aquamarine"}}>
             Front-End Developer
-            </h1>
+            </h2>
             <div> 
               <a href="https://www.linkedin.com/in/jacob-elbaz/" target="_blank" className='text-decoration-none'><img className="p_links" src={linkedin}/></a>
               <a href="https://github.com/JacobElbaz" target="_blank" className='text-decoration-none mx-3'><img className="p_links" src={github}/></a>
