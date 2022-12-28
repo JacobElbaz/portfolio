37a16043fdb494f5d75bb8a16d1eaa6bdd0294c5
JacobElbaz <jacobel@post.bgu.ac.il>
2022-12-28 12:08:51 +0200

    remove warnings

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
   return (
     <div className='App'>
       <NavigationBar />
-      <div class="header">
+      <div className="header">
 
         
-        <div class="inner-header flex">
+        <div className="inner-header flex">
         <Home />
       <About />
       <Skills />
@@ -23,12 +23,12 @@ function App() {
 
        
         <div>
-          <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-            viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+          <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
             <defs>
               <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
             </defs>
-            <g class="parallax">
+            <g className="parallax">
               <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(0,0,0,0.7" />
               <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(0,0,0,0.5)" />
               <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(0,0,0,0.3)" />
@@ -42,7 +42,7 @@ function App() {
      
 
     
-      <div class="content flex">
+      <div className="content flex">
       <Projects />
       <Contact />
       <Footer />
